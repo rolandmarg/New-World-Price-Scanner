e22041190f354756c17fde434d59c5dae997bba0
Roland <rmargve@gmail.com>
2022-10-27 15:55:23 -0700

    remove keybind coz fuck windows

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const screenshot = require('screenshot-desktop');
 const ocr = require('./ocr');
-const registerCallbackOnKey = require('./keyboardListener');
 
 async function bootstrap() {
   const worker = await ocr.createWorker();
 
-  const run = async () => {
-    const img = await screenshot({ format: 'png' });
-    const { data: { text } } = await worker.recognize(img);
-    console.log(text);
-  };
-
-  // await run();
-  registerCallbackOnKey('f10', run);
-  // await worker.terminate();
+  const img = await screenshot({ format: 'png' });
+  const { data: { text } } = await worker.recognize(img);
+  console.log(text);
 }
 
 bootstrap();
