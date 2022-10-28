7ddc8f8e34db0e3f7d5e530cd3fc42cdce2ec12d
Roland <rmargve@gmail.com>
2022-10-27 18:35:32 -0700

    use tesseract locally, drop that nodejs dep and almost switched to python

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const { execSync } = require('child_process');
 const screenshot = require('screenshot-desktop');
-const ocr = require('./ocr');
+const sharp = require('sharp');
+
+const sellPos = { left: 1622, top: 300, width: 200, height: 100 };
+const buyPos = { left: 1620, top: 705, width: 200, height: 100 };
+const itemPos = { left: 552, top: 304, width: 240, height: 32 };
 
 async function bootstrap() {
-  const worker = await ocr.createWorker();
-
   const img = await screenshot({ format: 'png' });
-  const { data: { text } } = await worker.recognize(img);
-  console.log(text);
+
+  await sharp(img).extract(itemPos).toFile('./screenshots/item.png');
+  await sharp(img).extract(sellPos).toFile('./screenshots/sell.png');
+  await sharp(img).extract(buyPos).toFile('./screenshots/buy.png');
+
+  console.log('item', execSync('tesseract screenshots\\item.png stdout', { encoding: 'utf8' }));
+  console.log('sell', execSync('tesseract screenshots\\sell.png stdout', { encoding: 'utf8' }));
+  console.log('buy', execSync('tesseract screenshots\\buy.png stdout', { encoding: 'utf8' }));
 }
 
 bootstrap();
